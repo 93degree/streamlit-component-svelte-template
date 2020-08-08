935f9ce714a682295c41e61e5ac6e13ef11bc422
Diego Rojo <rojo@hey.com>
2020-08-08 12:39:24 +0200

    Remove the main.ts (unexisting) props

--- my_component/frontend/src/main.ts
+++ my_component/frontend/src/main.ts
@@ -2,9 +2,6 @@ import MyComponent from "./MyComponent.svelte";
 
 const myComponent = new MyComponent({
   target: document.body,
-  props: {
-    name: "world",
-  },
 });
 
 export default myComponent;
